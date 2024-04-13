f7f10fb8747ebd612f1d768dbfd01b986216ef80
ji2chang <166942946+ji2chang@users.noreply.github.com>
2024-04-13 21:54:22 +0200

    Update World.hpp
    Utilizzo dell'algoritmo DDA per ottenere la distanza dai muri

--- src/core/World.hpp
+++ src/core/World.hpp
@@ -2,33 +2,75 @@
 // Created by marco on 17/03/2024.
 //
 
-#ifndef RAYLIB_TEMPLATE_PORTABLE_WINDOWS_WORLD_HPP
-#define RAYLIB_TEMPLATE_PORTABLE_WINDOWS_WORLD_HPP
+#include <valarray>
+#include "World.hpp"
 
-#include <memory>
-#include <raylib.h>
 
 namespace raycaster {
 
-    class World {
-    public:
-        World(const bool map[], int xSize, int ySize);
-        double RayTrace(Vector2 from, double angle);
+    World::World(const bool map[], int xSize, int ySize) {
+        m_Map = std::make_unique<bool[]>(xSize * ySize);
+        m_XSize = xSize;
+        m_YSize = ySize;
 
+        for (int i = 0; i < xSize * ySize; ++i) {
+            m_Map[i] = map[i];
+        }
 
-    private:
-        int m_XSize, m_YSize;
-        const double RAY_STEP = 0.05;
-        std::unique_ptr<bool[]> m_Map;
+    }
 
-        bool IsWall(int x, int y);
+    bool World::IsWall(int x, int y) {
+        return m_Map[y * m_XSize + x];
 
+    }
 
+    double World::RayTrace(Vector2 from, double angle) {
+        bool hit = false;
 
+        int mapX = (int)from.x;
+        int mapY = (int)from.y;
 
-    };
+        double dirX = cos(angle);
+        double dirY = sin(angle);
 
+        int stepX,stepY;
+        double deltaX = dirX == 0 ? 1e30 : std::abs(1/dirX);
+        double deltaY = dirY == 0 ? 1e30 : std::abs(1/dirY);
+        double sideDistX;
+        double sideDistY;
+
+        if(dirX < 0){
+            stepX = -1;
+            sideDistX = (from.x - mapX) * deltaX;
+        } else {
+            stepX = 1;
+            sideDistX = (mapX - from.x + 1.0) * deltaX;
+        }
+
+        if(dirY < 0){
+            stepY = -1;
+            sideDistY = (from.y - mapY) * deltaY;
+        } else {
+            stepY = 1;
+            sideDistY = (mapY - from.y + 1.0) * deltaY;
+        }
+        int side = 0;
+        while(!hit){
+            if(sideDistX < sideDistY){
+                sideDistX += deltaX;
+                mapX += stepX;
+                side = 0;
+            } else {
+                sideDistY += deltaY;
+                mapY += stepY;
+                side = 1;
+            }
+            hit = IsWall(mapX,mapY);
+        }
+        if(side == 0){
+            return sideDistX - deltaX;
+        } else {
+            return sideDistY - deltaY;
+        }
+    }
 } // raycaster
-
-
-#endif //RAYLIB_TEMPLATE_PORTABLE_WINDOWS_WORLD_HPP
